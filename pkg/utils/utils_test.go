45926b97a9dadbb2f8fc023362cdb72cd0b2a846
Joel Takvorian <joel.takvorian@qaraywa.net>
2024-03-18 09:49:03 +0100

    NETOBSERV-559: use LookupAndDelete to read maps (#283)
    Keep legacy code for old kernels
    
    Do not base support detection on kernel version
    
    Instead, just try and fallback to legacy when relevant

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,52 +1,50 @@
 package utils
 
 import (
-	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestIskernelOlderthan514(t *testing.T) {
 	tests := []struct {
-		name              string
-		mockKernelVersion func() (uint32, error)
-		want              bool
+		name          string
+		kernelVersion string
+		want          bool
+		wantError     bool
 	}{
 		{
-			name: "Kernel version < 5.14.0",
-			mockKernelVersion: func() (uint32, error) {
-				ver, _ := kernelVersionFromReleaseString("5.13.0")
-				return ver, nil
-			},
-			want: true,
+			name:          "Kernel version < 5.14.0",
+			kernelVersion: "5.13.0",
+			want:          true,
 		},
 		{
-			name: "Kernel version = 5.14.0",
-			mockKernelVersion: func() (uint32, error) {
-				ver, _ := kernelVersionFromReleaseString("5.14.0")
-				return ver, nil
-			},
-			want: false,
+			name:          "Kernel version = 5.14.0",
+			kernelVersion: "5.14.0",
+			want:          false,
 		},
 		{
-			name: "Kernel version > 5.14.0",
-			mockKernelVersion: func() (uint32, error) {
-				ver, _ := kernelVersionFromReleaseString("5.15.0")
-				return ver, nil
-			},
-			want: false,
+			name:          "Kernel version > 5.14.0",
+			kernelVersion: "5.15.0",
+			want:          false,
 		},
 		{
-			name: "Error getting kernel version",
-			mockKernelVersion: func() (uint32, error) {
-				return 0, errors.New("error")
-			},
-			want: false,
+			name:          "Error getting kernel version",
+			kernelVersion: "invalid version",
+			want:          false,
+			wantError:     true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			getCurrentKernelVersion = tt.mockKernelVersion
-			got := IskernelOlderthan514()
+			versionUint, err := kernelVersionFromReleaseString(tt.kernelVersion)
+			if tt.wantError {
+				require.Errorf(t, err, "%s: expecting error, got none", tt.name)
+			} else {
+				require.NoErrorf(t, err, "%s: expecting no error, got %v", tt.name, err)
+			}
+			kernelVersion = versionUint
+			got := IsKernelOlderThan("5.14.0")
 			if got != tt.want {
 				t.Errorf("%s: IskernelOlderthan514() = %v, want %v", tt.name, got, tt.want)
 			}
