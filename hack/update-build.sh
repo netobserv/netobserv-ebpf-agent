46d341b902f28fde1ae00da9f708cffdc13c470e
Olivier Cazade <ocazade@redhat.com>
2024-09-12 15:22:31 +0000

    Addressing konflux error and fixing multi arch build with podman 5.2

--- hack/update-build.sh
+++ hack/update-build.sh
@@ -16,3 +16,5 @@ LABEL io.openshift.tags="network-observability-ebpf-agent"
 LABEL upstream-vcs-ref="${COMMIT}"
 LABEL upstream-vcs-type="git"
 EOF
+
+sed -i 's/\(FROM.*\)docker.io\/library\/golang:1.22\(.*\)/\1brew.registry.redhat.io\/rh-osbs\/openshift-golang-builder:v1.22.5-202407301806.g4c8b32d.el9\2/g' ./Dockerfile
