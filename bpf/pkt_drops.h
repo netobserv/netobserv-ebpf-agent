103764f944872ab1226e23348cf169dec6a779c7
Mohamed Mahmoud <mmahmoud@redhat.com>
2024-09-26 07:35:17 -0400

    NETOBSERV-1896: refactor pkt drop to be more efficient with using stack
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- bpf/pkt_drops.h
+++ bpf/pkt_drops.h
@@ -7,41 +7,60 @@
 
 #include "utils.h"
 
+static inline long pkt_drop_lookup_and_update_flow(flow_id *id, u8 state, u16 flags,
+                                                   enum skb_drop_reason reason, u64 len) {
+    flow_metrics *aggregate_flow = bpf_map_lookup_elem(&aggregated_flows, id);
+    if (aggregate_flow != NULL) {
+        aggregate_flow->end_mono_time_ts = bpf_ktime_get_ns();
+        aggregate_flow->pkt_drops.packets += 1;
+        aggregate_flow->pkt_drops.bytes += len;
+        aggregate_flow->pkt_drops.latest_state = state;
+        aggregate_flow->pkt_drops.latest_flags = flags;
+        aggregate_flow->pkt_drops.latest_drop_cause = reason;
+        long ret = bpf_map_update_elem(&aggregated_flows, id, aggregate_flow, BPF_EXIST);
+        if (trace_messages && ret != 0) {
+            bpf_printk("error packet drop updating flow %d\n", ret);
+        }
+        return 0;
+    }
+    return -1;
+}
+
 static inline int trace_pkt_drop(void *ctx, u8 state, struct sk_buff *skb,
                                  enum skb_drop_reason reason) {
     flow_id id;
     __builtin_memset(&id, 0, sizeof(id));
 
-    u8 protocol = 0;
+    u8 protocol = 0, dscp = 0;
     u16 family = 0, flags = 0;
 
-    id.if_index = skb->skb_iif;
+    id.if_index = BPF_CORE_READ(skb, skb_iif);
     // filter out TCP sockets with unknown or loopback interface
     if (id.if_index == 0 || id.if_index == 1) {
         return 0;
     }
     // read L2 info
-    set_key_with_l2_info(skb, &id, &family);
+    core_fill_in_l2(skb, &id, &family);
 
     // read L3 info
-    set_key_with_l3_info(skb, family, &id, &protocol);
+    core_fill_in_l3(skb, &id, family, &protocol, &dscp);
 
     // read L4 info
     switch (protocol) {
     case IPPROTO_TCP:
-        set_key_with_tcp_info(skb, &id, protocol, &flags);
+        core_fill_in_tcp(skb, &id, &flags);
         break;
     case IPPROTO_UDP:
-        set_key_with_udp_info(skb, &id, protocol);
+        core_fill_in_udp(skb, &id);
         break;
     case IPPROTO_SCTP:
-        set_key_with_sctp_info(skb, &id, protocol);
+        core_fill_in_sctp(skb, &id);
         break;
     case IPPROTO_ICMP:
-        set_key_with_icmpv4_info(skb, &id, protocol);
+        core_fill_in_icmpv4(skb, &id);
         break;
     case IPPROTO_ICMPV6:
-        set_key_with_icmpv6_info(skb, &id, protocol);
+        core_fill_in_icmpv6(skb, &id);
         break;
     default:
         return 0;
@@ -52,11 +71,11 @@ static inline int trace_pkt_drop(void *ctx, u8 state, struct sk_buff *skb,
     if (skip) {
         return 0;
     }
-
+    u64 len = BPF_CORE_READ(skb, len);
     long ret = 0;
     for (direction dir = INGRESS; dir < MAX_DIRECTION; dir++) {
         id.direction = dir;
-        ret = pkt_drop_lookup_and_update_flow(skb, &id, state, flags, reason);
+        ret = pkt_drop_lookup_and_update_flow(&id, state, flags, reason, len);
         if (ret == 0) {
             return 0;
         }
@@ -69,7 +88,7 @@ static inline int trace_pkt_drop(void *ctx, u8 state, struct sk_buff *skb,
         .end_mono_time_ts = current_time,
         .flags = flags,
         .pkt_drops.packets = 1,
-        .pkt_drops.bytes = skb->len,
+        .pkt_drops.bytes = len,
         .pkt_drops.latest_state = state,
         .pkt_drops.latest_flags = flags,
         .pkt_drops.latest_drop_cause = reason,
@@ -84,16 +103,12 @@ static inline int trace_pkt_drop(void *ctx, u8 state, struct sk_buff *skb,
 
 SEC("tracepoint/skb/kfree_skb")
 int kfree_skb(struct trace_event_raw_kfree_skb *args) {
-    struct sk_buff skb;
-
     if (do_sampling == 0) {
         return 0;
     }
 
-    __builtin_memset(&skb, 0, sizeof(skb));
-
-    bpf_probe_read(&skb, sizeof(struct sk_buff), args->skbaddr);
-    struct sock *sk = skb.sk;
+    struct sk_buff *skb = (struct sk_buff *)BPF_CORE_READ(args, skbaddr);
+    struct sock *sk = (struct sock *)BPF_CORE_READ(skb, sk);
     enum skb_drop_reason reason = args->reason;
 
     // SKB_NOT_DROPPED_YET,
@@ -105,7 +120,7 @@ int kfree_skb(struct trace_event_raw_kfree_skb *args) {
             // pull in details from the packet headers and the sock struct
             bpf_probe_read(&state, sizeof(u8), (u8 *)&sk->__sk_common.skc_state);
         }
-        return trace_pkt_drop(args, state, &skb, reason);
+        return trace_pkt_drop(args, state, skb, reason);
     }
     return 0;
 }
