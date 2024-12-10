d940ac2dbb324edc16c633ceca78aa3749825949
Joel Takvorian <jtakvori@redhat.com>
2024-12-10 11:57:57 +0100

    Use correct tag for ghaction builds

--- hack/build-bytecode-images-multi.sh
+++ hack/build-bytecode-images-multi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Set default value for IMAGE_NOO_BC tag if not already set
-IMAGE_NOO_BC=${IMAGE_NOO_BC:-quay.io/netobserv/ebpf-bytecode}
+IMAGE_NOO_BC=${IMAGE_NOO_BC:-quay.io/${USER}/ebpf-bytecode}
+OCI_BIN=${OCI_BIN:-docker}
 
 # PROGRAMS is a list of <program name>:<program type> tuples
 PROGRAMS='{
@@ -31,17 +32,36 @@ MAPS='{
 "filter_map":"lpm_trie"
 }'
 
-docker buildx create --use
-docker buildx inspect --bootstrap
+if [[ ${OCI_BIN} == "docker" ]]; then
+  docker buildx create --name bytecode-builder --use
+  docker buildx inspect --bootstrap
 
-DOCKER_BUILDKIT=1 docker buildx build \
- --platform linux/amd64,linux/arm64,linux/s390x,linux/ppc64le \
- --build-arg PROGRAMS="$PROGRAMS" \
- --build-arg MAPS="$MAPS" \
- --build-arg BC_AMD64_EL=bpf_x86_bpfel.o \
- --build-arg BC_ARM64_EL=bpf_arm64_bpfel.o \
- --build-arg BC_S390X_EB=bpf_s390_bpfeb.o \
- --build-arg BC_PPC64LE_EL=bpf_powerpc_bpfel.o \
- -f ./Containerfile.bytecode.multi.arch \
- --push \
- ./pkg/ebpf -t $IMAGE_NOO_BC
+  docker buildx build \
+  --platform linux/amd64,linux/arm64,linux/s390x,linux/ppc64le \
+  --build-arg PROGRAMS="$PROGRAMS" \
+  --build-arg MAPS="$MAPS" \
+  --build-arg BC_AMD64_EL=bpf_x86_bpfel.o \
+  --build-arg BC_ARM64_EL=bpf_arm64_bpfel.o \
+  --build-arg BC_S390X_EB=bpf_s390_bpfeb.o \
+  --build-arg BC_PPC64LE_EL=bpf_powerpc_bpfel.o \
+  ${OCI_BUILD_OPTS} \
+  -f ./Containerfile.bytecode.multi.arch \
+  --push \
+  ./pkg/ebpf -t $IMAGE_NOO_BC
+
+  docker buildx rm bytecode-builder
+else
+  ${OCI_BIN} buildx build \
+  --platform linux/amd64,linux/arm64,linux/s390x,linux/ppc64le \
+  --build-arg PROGRAMS="$PROGRAMS" \
+  --build-arg MAPS="$MAPS" \
+  --build-arg BC_AMD64_EL=bpf_x86_bpfel.o \
+  --build-arg BC_ARM64_EL=bpf_arm64_bpfel.o \
+  --build-arg BC_S390X_EB=bpf_s390_bpfeb.o \
+  --build-arg BC_PPC64LE_EL=bpf_powerpc_bpfel.o \
+  ${OCI_BUILD_OPTS} \
+  -f ./Containerfile.bytecode.multi.arch \
+  ./pkg/ebpf -t $IMAGE_NOO_BC
+
+  ${OCI_BIN} push $IMAGE_NOO_BC
+fi
